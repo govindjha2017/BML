7e5142f7b6157066518fa8ff89568fbcbc3cc38c
govind <govindkumarjha2017@gmail.com>
2026-09-25 14:53:22 +0530

    25 sept

---
+++ CSE-VI/L12/introToJs/array.js
@@ -0,0 +1,24 @@
+
+// var arr = [34,11,56,71,"Hello"];
+
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr.indexOf(56))
+
+
+var arr = [34,11,56,71,"Hello"];
+
+console.log("before",arr);
+// arr.push(33);
+// arr.pop();
+// arr.pop();
+
+// arr.unshift("start");
+// arr.shift()
+
+// arr.splice(2,1)
+// arr.splice(1,3)
+// arr.splice(2,0,51,61)
+arr.splice(2,1,43,63)
+
+console.log("after",arr);
---
+++ CSE-VI/L12/introToJs/script.js
@@ -0,0 +1,38 @@
+
+// console.log("Hello Ji");
+
+// // var let const 
+
+// var a =10;
+// var a = "Hello"
+// var a;
+// a=11;
+// var a = 13;
+
+// console.log(a);
+
+
+// let b = 30;
+// // let b; can't do this
+// b=55;
+// console.log(b);
+
+// const c = 100;
+// // const c  can't do this
+// // c=150;  can't do this
+
+// console.log(c);
+
+
+{ 
+    var x=11;
+    let y=12;
+    const z=13;
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+console.log(x);
+// console.log(y);
+// console.log(z);
